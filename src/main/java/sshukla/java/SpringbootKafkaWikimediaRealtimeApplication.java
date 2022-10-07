6fb12268c3b39e881f56e07f4d6d8bd817488342
Seemant Shukla <13106945+sshukla154@users.noreply.github.com>
2022-10-07 23:47:26 +0200

    Initial Setup

---
+++ src/main/java/sshukla/java/SpringbootKafkaWikimediaRealtimeApplication.java
@@ -0,0 +1,13 @@
+package sshukla.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaWikimediaRealtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaWikimediaRealtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sshukla/java/SpringbootKafkaWikimediaRealtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package sshukla.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaWikimediaRealtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
